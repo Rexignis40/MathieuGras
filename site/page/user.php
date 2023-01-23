e69a4d9cf1c25d7f77530414166c07246621620f
Fusix10 <acamandona@gaming.tech>
2023-01-23 15:20:10 +0100

    formulaire du pauvre

---
+++ site/page/user.php
@@ -0,0 +1,41 @@
+<?php require_once "config.php" ?>
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<h1>vous vous êtes jamais inscrit ?, quelllllllllllle honte !, veuillez y remédier</h1>
+<form method="post" action="singe-up.php">
+    <p>votre Prénom Nom</p>
+    <input type='text' name='name' />
+    <p>pegi 18 ?</p>
+    <input type='number' name='age' />
+    <p>votre E mail/girl</p>
+    <input type='email' name='email'/>
+    <p>votre mot de passerait pas !</p>
+    <input type='password' name='password' />
+    <p>votre numéro de carte bancaire</p>
+    <input type='tel' name='num' />
+    <p>votre adresse</p>
+    <input type='text' name='adresse' />
+    <input type='submit' value='Créer un compte' />
+</form>
+<h2>HOPOOOOOOO, vous êtes CONCITOYEN, bien le retour !</h2>
+<form method="post" action="singe-in.php">
+    <p>votre pseudo</p>
+    <input type='name' name='name' />
+    <p>votre mot de passerait pas !</p>
+    <input type='password' name='password' />
+    <p>Me connecter</p>
+    <input type='submit' value='Me connecter' />
+
+</form>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript">
+<?php if (isset($_SESSION['error'])){
+        echo "M.toast({html: '".$_SESSION['error']."'})";
+        unset($_SESSION['error']); 
+} ?>  
+</script>
+</body>
+</html>
