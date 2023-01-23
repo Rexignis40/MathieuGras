8e06d90b9e19344c20dfa574502717864a39ddd9
Fusix10 <acamandona@gaming.tech>
2023-01-23 15:39:15 +0100

    sha1^5555555555

--- site/php/login.php
+++ site/php/login.php
@@ -11,7 +11,7 @@ if(empty($_POST['name'])){
 $sql = "SELECT * FROM user WHERE name=:name AND password=sha1(:password)"; 
 $pre = $pdo->prepare($sql); 
 $pre->bindParam(":name",$_POST['name']);
-$pre->bindValue(":password",sha1($_POST['password']."vdvfdgdf234567890°+£µ*%MPHBN?KOYGHBsgvdf788383fdvfdf4894891154bdfbdsvdfv")."4g8rez48v4rfbg56re4b5fd78hf5b4reh486fe4g8re8");
+$pre->bindValue(":password",sha1(sha1(sha1(sha1(sha1(sha1($_POST['password']."vdvfdgdf234567890°+£µ*%MPHBN?KOYGHBsgvdf788383fdvfdf4894891154bdfbdsvdfv")."4g8rez48v4rfbg56re4b5fd78hf5b4reh486fe4g8re8"))))));
 $pre->execute();
 $user = $pre->fetch(PDO::FETCH_ASSOC);
 if(empty($user)){
