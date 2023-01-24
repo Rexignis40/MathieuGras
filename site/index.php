8252edd5f72a85bf038c7ba0de93d68f4e2b3daf
doriansimonet <dsimonet@gaming.tech>
2023-01-24 13:13:08 +0100

    contact

--- site/index.php
+++ site/index.php
@@ -7,6 +7,9 @@
     <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Maven+Pro:wght@500&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
     <title>Mathieu Gras Photography</title>
 </head>
 <body>
