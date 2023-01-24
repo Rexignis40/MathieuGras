0dafdc6f51841a60f62456d638cd26b827d2d53e
Rexignis40 <61108765+Rexignis40@users.noreply.github.com>
2023-01-24 13:31:19 +0100

    GetImg

--- site/php/getImg.php
+++ site/php/getImg.php
@@ -1,5 +1,21 @@
 <?php
 require_once "config.php";
 
+$sql = "SELECT id, name, price FROM image";
+if(isset($_GET["c"])){
+    $sql .= " WHERE category=:category";
+}
+$offset = 0;
+if(isset($_GET["o"])){
+    $offset = $_GET["o"];
+}
 
+$q = $dbo->prepare($sql . " LIMIT :offset");
+$q->bindParam(":offset", $offset);
+if(isset($_GET["c"])){
+    $q->bindParam(":category", $_GET["c"], PDO::PARAM_INT);
+}
+$result = $q->execute();
+
+return $result->fetchAll();
 ?>
