68c7dc79df60f29f2830161272cfbc64c0100e2c
ShulingK <timbert@gaming.tech>
2023-01-25 16:14:53 +0100

    carousel index finit :)

--- site/index.php
+++ site/index.php
@@ -7,32 +7,32 @@
     <div class="carousel carousel-slider center row">
         <div class="carousel-item white-text carouselOne " href="">
             <p class="textOne">
-                Photographe, guitariste et pianiste, j’ai grandi dans le sud de la France, en Provence.<br>-<br>
-                J’ai commencé à me plonger dans la photographie dès mon plus jeune âge. Artiste dans l’âme, j’aime déambuler dans les villes de Provence afin de capturer un instant, une époque et d’en retranscrire un témoignage à travers mes photos. Extrêmement curieux, je suis constamment à la recherche de nouveaux projets.<br>-<br>
+                Photographe, guitariste et pianiste, j’ai grandi dans le sud de la France, en Provence.<br><br>
+                J’ai commencé à me plonger dans la photographie dès mon plus jeune âge. Artiste dans l’âme, j’aime déambuler dans les villes de Provence afin de capturer un instant, une époque et d’en retranscrire un témoignage à travers mes photos. Extrêmement curieux, je suis constamment à la recherche de nouveaux projets.<br><br>
                 Mes domaines de prédilection sont le portrait, le nu artistique et la street photography.
             </p>
         </div>
 
         <div class="carousel-item white-text carouselTwo" href="">
             <p class="textTwo">
-                Photographe, guitariste et pianiste, j’ai grandi dans le sud de la France, en Provence.<br>-<br>
-                J’ai commencé à me plonger dans la photographie dès mon plus jeune âge. Artiste dans l’âme, j’aime déambuler dans les villes de Provence afin de capturer un instant, une époque et d’en retranscrire un témoignage à travers mes photos. Extrêmement curieux, je suis constamment à la recherche de nouveaux projets.<br>-<br>
+                Photographe, guitariste et pianiste, j’ai grandi dans le sud de la France, en Provence.<br><br>
+                J’ai commencé à me plonger dans la photographie dès mon plus jeune âge. Artiste dans l’âme, j’aime déambuler dans les villes de Provence afin de capturer un instant, une époque et d’en retranscrire un témoignage à travers mes photos. Extrêmement curieux, je suis constamment à la recherche de nouveaux projets.<br><br>
                 Mes domaines de prédilection sont le portrait, le nu artistique et la street photography.
             </p>
         </div>
 
         <div class="carousel-item white-text carouselThree" href="">
             <p class="textThree">
-                Photographe, guitariste et pianiste, j’ai grandi dans le sud de la France, en Provence.<br>-<br>
-                J’ai commencé à me plonger dans la photographie dès mon plus jeune âge. Artiste dans l’âme, j’aime déambuler dans les villes de Provence afin de capturer un instant, une époque et d’en retranscrire un témoignage à travers mes photos. Extrêmement curieux, je suis constamment à la recherche de nouveaux projets.<br>-<br>
+                Photographe, guitariste et pianiste, j’ai grandi dans le sud de la France, en Provence.<br><br>
+                J’ai commencé à me plonger dans la photographie dès mon plus jeune âge. Artiste dans l’âme, j’aime déambuler dans les villes de Provence afin de capturer un instant, une époque et d’en retranscrire un témoignage à travers mes photos. Extrêmement curieux, je suis constamment à la recherche de nouveaux projets.<br><br>
                 Mes domaines de prédilection sont le portrait, le nu artistique et la street photography.
             </p>
         </div>
 
         <div class="carousel-item white-text carouselFour" href="">
             <p class="textFour">
-                Photographe, guitariste et pianiste, j’ai grandi dans le sud de la France, en Provence.<br>-<br>
-                J’ai commencé à me plonger dans la photographie dès mon plus jeune âge. Artiste dans l’âme, j’aime déambuler dans les villes de Provence afin de capturer un instant, une époque et d’en retranscrire un témoignage à travers mes photos. Extrêmement curieux, je suis constamment à la recherche de nouveaux projets.<br>-<br>
+                Photographe, guitariste et pianiste, j’ai grandi dans le sud de la France, en Provence.<br><br>
+                J’ai commencé à me plonger dans la photographie dès mon plus jeune âge. Artiste dans l’âme, j’aime déambuler dans les villes de Provence afin de capturer un instant, une époque et d’en retranscrire un témoignage à travers mes photos. Extrêmement curieux, je suis constamment à la recherche de nouveaux projets.<br><br>
                 Mes domaines de prédilection sont le portrait, le nu artistique et la street photography.
             </p>
         </div>
