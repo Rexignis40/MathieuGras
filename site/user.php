29c2c329f42f252e351f630279fc3c11ce03a282
doriansimonet <dsimonet@gaming.tech>
2023-01-26 16:33:54 +0100

    inscription

--- site/user.php
+++ site/user.php
@@ -2,13 +2,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+<?php include('./components/head.html')?>
 </head>
+<body id="use">
+<?php include('./components/header.html')?>
 <?php 
 if(isset($_SESSION["user"])){
     ?>
-    <h1>VOUS ETES GROS</h1>
-    <h2> vos infos personelle:</h2>
+    <h1> vos infos personelle:</h1>
     <?php echo $_SESSION["user"]['name'] ?>
     <form method="post" action="php/actions/uptateUser.php" enctype="multipart/form-data">
         <h2>Pseudo</h2>
@@ -38,35 +39,25 @@ if(isset($_SESSION["user"])){
 }
 else{
     ?>
-    <h1>vous vous êtes jamais inscrit ?, quelllllllllllle honte !, veuillez y remédier</h1>
-<form method="post" action="php/logup.php">
-    <p>votre Prénom Nom</p>
-    <input type='text' name='name' />
-    <p>pegi 18 ?</p>
-    <input type='number' name='age' />
-    <p>votre E mail/girl</p>
-    <input type='email' name='email'/>
-    <p>votre mot de passerait pas !</p>
-    <input type='password' name='password' />
-    <p>votre numéro de carte bancaire</p>
-    <input type='tel' name='num' />
-    <p>votre adresse</p>
-    <input type='text' name='adresse' />
-    <input type='submit' value='Créer un compte' />
-</form>
-<h2>HOPOOOOOOO, vous êtes CONCITOYEN, bien le retour !</h2>
-<form method="post" action="php/login.php">
-    <p>votre pseudo</p>
-    <input type='name' name='name' />
-    <p>votre mot de passerait pas !</p>
-    <input type='password' name='password' />
-    <p>Me connecter</p>
-    <input type='submit' value='Me connecter' />
-
-</form>
+    <div class="row">
+        <div class="connection col l4 offset-l4 ">
+            <h2>SE CONNECTER</h2>
+            <div class="test">
+                <form method="post" action="php/login.php">
+                    <p>Pseudo/E-mail</p>
+                    <input class="white-text" type='text' name='name' />
+                    <p>Mot de passe</p>
+                    <input class="white-text" type='password' name='password' />
+                    <div class="count"><input class="butonuser" type='submit' value='Me connecter' /></div>
+                </form>
+            </div>
+            <a href="inscription.php">S'inscrire</a>
+        </div>
+    </div>
 <?php
 }
 ?>
+    <?php include('./components/footer.html')?>
 <script type="text/javascript" src="../js/Jquery.min.js"></script>
 <script type="text/javascript" src="../js/materialize.min.js"></script>
 <script type="text/javascript" src="../js/script.js"></script>
