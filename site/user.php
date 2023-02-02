4b3f44bea6d1182186387a8e544096ddeb6e8790
ShulingK <timotheimbert3@gmail.com>
2023-02-02 23:26:51 +0100

    need pull

--- site/user.php
+++ site/user.php
@@ -36,13 +36,13 @@ if(isset($_SESSION["user"])){
         ?>
         <div class="createNewPrest">
             <h3>Nouvelle préstation</h3>
-            <h2>Titre</h2>
+            <h4>Titre</h4>
             <input class="newPrest" name="title" type='text' id="title">
-            <h2>Description</h2>
+            <h4>Description</h4>
             <input class="newPrest" name="desc" type="text" id="description">
-            <h2>Prix de la préstation</h2>
+            <h4>Prix de la préstation</h4>
             <input class="newPrest" name="price" type="text" id="price">
-            <h2>Images</h2>
+            <h4>Images</h4>
             <input name="img1" type="file" id="img1">
             <input name="img2" type="file" id="img2">
             <button onClick="SetPrestation()">Envoyer</button>
@@ -55,11 +55,11 @@ if(isset($_SESSION["user"])){
             <label>4: <input class="newCategory" type="radio" name="which-carousel" value="3"></label>
             <label>5: <input class="newCategory" type="radio" name="which-carousel" value="4"></label>
             <label>6: <input class="newCategory" type="radio" name="which-carousel" value="5"></label>
-            <h2>Titre</h2>
+            <h4>Titre</h4>
             <input class="newCategory" name="title" type='text' id="title-portfolio">
-            <h2>Explication du projet</h2>
+            <h4>Explication du projet</h4>
             <input class="newCategory" name="desc" type="text" id="description-portfolio">
-            <h2>Images</h2>
+            <h4>Images</h4>
             <input name="img1" type="file" id="img1-portfolio">
             <input name="img2" type="file" id="img2-portfolio">
             <input name="img3" type="file" id="img3-portfolio">
